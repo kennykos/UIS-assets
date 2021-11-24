0b4927c7edd5b4e2142442839aed596e6f0cfb66
gkk2 <gkk2@illinois.edu>
2021-11-24 12:28:12 -0600

    SQL up and running

--- investments.py
+++ investments.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 import os
+import csv
 
 
 # Connecting to the database
@@ -9,26 +10,28 @@ conn = sql.connect('investments.db')
 c = conn.cursor()
 
 
-# creating our table if our table dose not exist
-try:
-    c.execute("""CREATE TABLE investments (
-                account text,
-                cupon real,
-                maturity_data text,
-                quantity integer,
-                cost_value real,
-                market_value real,
-                industry text,
-                fossil_fuel integer,
-                ticker text,
-                holding_class text,
-                bank text
-                )""")
-except:
-    print("table already exists")
+# Deleting our table if it already exists
+c.execute("DROP TABLE IF EXISTS investments")
+# creating our table
+c.execute("""CREATE TABLE investments (
+            account text,
+            cupon real,
+            maturity_data text,
+            quantity integer,
+            cost_value real,
+            market_value real,
+            industry text,
+            fossil_fuel integer,
+            ticker text,
+            asset_class text,
+            bank text
+            )""")
 
 # assign directory
 directory = 'datasets2021'
+
+# list of asset class types
+asset_class_lst = ["Asset-Backed Securities", "Corporate Bonds", "U.S. Agency Bonds", "U.S. Treasury Securities"]
  
 # iterate over files in
 # that directory
@@ -36,11 +39,27 @@ for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     # checking if it is a file
     if os.path.isfile(f):
-        print(f)
-
-
-
-
+        # get the name of the bank for each file
+        bank = f[f.rfind('-') + 1 : f.rfind(".")]
+        with open(f) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                # do something
+                asset_class = " "
+                if row[0] in asset_class_lst:
+                    # get the asset class subgroup
+                    asset_class = row[0]
+                account = row[0]            #type text
+                cupon = row[1]
+                maturity_data = row[2]
+                quantity = row[3]
+                cost_value = row[4]
+                market_value = row[5]
+                industry = row[6]
+                fossil_fuel = row[7]
+                ticker = row[8]
+                c.execute("""INSERT INTO investments(account, cupon, maturity_data, quantity, cost_value, market_value, industry, fossil_fuel, ticker, asset_class, bank)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?)""", (account, cupon, maturity_data, quantity, cost_value, market_value, industry, fossil_fuel, ticker, asset_class, bank))
 # commiting the changes to the database
 conn.commit()
 
